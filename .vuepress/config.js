7ff622474304bff8a91c706fd597cfe625169edb
Isaac Levin <isaac.r.levin@gmail.com>
2020-10-20 12:45:06 -0700

    test

---
+++ .vuepress/config.js
@@ -0,0 +1,164 @@
+const currentDateUTC = new Date().toUTCString()
+
+module.exports = {
+	title: 'Azure Tips and Tricks',
+	dest: './public',
+	base: '/AzureTipsAndTricks/',
+	markdown: {
+		lineNumbers: true
+	},
+	themeConfig: {
+		domain: 'http://azuredev.tips',
+		displayAllHeaders: true,
+		sidebar: 'auto',
+		searchMaxSuggestions: 100,
+		repo: 'microsoft/azuretipsandtricks',
+		repoLabel: 'Star this Repo',
+		editLinks: true,
+		editLinkText: 'Edit this page on GitHub',
+		logo: '/files/vintage.png',
+		sidebar: [
+			{
+				title: 'Home',
+				collapsable: false,
+				children: [
+					'/'
+				]
+			}
+		],
+		nav: [
+			{ text: 'Home', link: '/' },
+			{ text: 'Videos', link: 'http://videos.azuredev.tips' },
+			{ text: 'Questions?', link: 'https://github.com/Microsoft/AzureTipsAndTricks/issues/' },
+			{ text: 'RSS Feed', link: 'https://microsoft.github.io/AzureTipsAndTricks/rss.xml' }
+		]//,
+		//	logo: '/vintage.png'
+	},
+	plugins: [
+		[
+			'@vuepress/search',
+			{
+				searchMaxSuggestions: 10
+			}
+		],
+		[
+			'vuepress-plugin-rss',
+			{
+				site_url: 'https://microsoft.github.io/AzureTipsAndTricks',
+				copyright: '2020 Microsoft',
+				filter: frontmatter => frontmatter.date <= new Date(currentDateUTC),
+				count: 20
+			}
+		],
+		'vuepress-plugin-janitor'
+	],
+	head: [
+		['script', {}, `
+			(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+			new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+			j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+			'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+			})(window,document,'script','dataLayer','GTM-TDHW3GW');
+		`],
+		[
+			'link',
+			{ rel: 'apple-touch-icon', sizes: '57x57', href: '/apple-icon-57x57.png' }
+		],
+		[
+			'link',
+			{ rel: 'apple-touch-icon', sizes: '60x60', href: '/apple-icon-60x60.png' }
+		],
+		[
+			'link',
+			{ rel: 'apple-touch-icon', sizes: '72x72', href: '/apple-icon-72x72.png' }
+		],
+		[
+			'link',
+			{ rel: 'apple-touch-icon', sizes: '76x76', href: '/apple-icon-76x76.png' }
+		],
+		[
+			'link',
+			{
+				rel: 'apple-touch-icon',
+				sizes: '114x114',
+				href: '/apple-icon-114x114.png'
+			}
+		],
+		[
+			'link',
+			{
+				rel: 'apple-touch-icon',
+				sizes: '120x120',
+				href: '/apple-icon-120x120.png'
+			}
+		],
+		[
+			'link',
+			{
+				rel: 'apple-touch-icon',
+				sizes: '144x144',
+				href: '/apple-icon-144x144.png'
+			}
+		],
+		[
+			'link',
+			{
+				rel: 'apple-touch-icon',
+				sizes: '152x152',
+				href: '/apple-icon-152x152.png'
+			}
+		],
+		[
+			'link',
+			{
+				rel: 'apple-touch-icon',
+				sizes: '180x180',
+				href: '/apple-icon-180x180.png'
+			}
+		],
+		[
+			'link',
+			{
+				rel: 'icon',
+				type: 'image/png',
+				sizes: '192x192',
+				href: '/android-icon-192x192.png'
+			}
+		],
+		[
+			'link',
+			{
+				rel: 'icon',
+				type: 'image/png',
+				sizes: '32x32',
+				href: '/favicon-32x32.png'
+			}
+		],
+		[
+			'link',
+			{
+				rel: 'icon',
+				type: 'image/png',
+				sizes: '96x96',
+				href: '/favicon-96x96.png'
+			}
+		],
+		[
+			'link',
+			{
+				rel: 'icon',
+				type: 'image/png',
+				sizes: '16x16',
+				href: '/favicon-16x16.png'
+			}
+		],
+		['link', { rel: 'manifest', href: '/manifest.json' }],
+		['meta', { name: 'msapplication-TileColor', content: '#ffffff' }],
+		[
+			'meta',
+			{ name: 'msapplication-TileImage', content: '/ms-icon-144x144.png' }
+		],
+		['meta', { name: 'theme-color', content: '#ffffff' }],
+		['script', { async: true, src: "https://platform.twitter.com/widgets.js", charset: "utf-8" }]
+	]
+}
---
+++ stickers/README.md
@@ -0,0 +1,44 @@
+---
+type: post
+title: "Azure Tips and Tricks Artwork"
+excerpt: "Print stickers of Azure Tips and Tricks"
+tags: [azure, stickers]
+share: true
+date: 2019-03-07 17:00:00
+---
+
+## Readme - Azure Tips and Tricks Artwork
+----------------------
+
+Based on the written blog post series of [Azure Tips and Tricks](http://azuredev.tips/). This is a collection of stickers that I've used at events and community meetups - now available to you.
+
+## How to print?
+
+Select the design that you want and use [Sticker Mule](https://www.stickermule.com/) to print high-quality, color correct, stickers.
+
+## Software and File Type used
+
+The application used to create all of the images was Adobe Illustrator. After creation, we exported to EPS so that transferring image data between different operating systems would be easier. The EPS file type is supported by several different drawing programs and vector graphic editing applications.
+
+## Collage
+
+**NOTE :** The bleed line shown in the images below is for the printer. It will not appear in the printed stickers.
+
+<img src="/AzureTipsAndTricks//stickers/preview/lightbulb.png" alt="drawing"  height="350" /> <img src="/AzureTipsAndTricks/stickers/preview/seattle.png" alt="drawing" height="350"/>
+
+* [Download Lightbulb](/AzureTipsAndTricks/stickers/2x2-circle-lightbulb.eps)
+* [Download Seattle Skyline](/AzureTipsAndTricks/stickers/2x2-circle-seattle.eps)
+
+<img src="/AzureTipsAndTricks/stickers/preview/vintage.png" alt="drawing" height="350" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src="/AzureTipsAndTricks/stickers/preview/8bit.png" alt="drawing" height="350"/>
+
+* [Download Vintage](/AzureTipsAndTricks/blob/stickers/2x2-circle-vintage.eps)
+* [Download 8-bit](/AzureTipsAndTricks/stickers/2x2-rounded-square-8-bit.eps)
+
+<img src="/AzureTipsAndTricks/stickers/preview/shark.png" alt="drawing" width="350" /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src="/AzureTipsAndTricks/stickers/preview/boggle.png" alt="drawing" height="350"/>
+
+* [Download Shark](/AzureTipsAndTricks/stickers/2x2-rounded-shark-laser-eyes.eps)
+* [Download Boggle](/AzureTipsAndTricks/stickers/2x2-rounded-boggle.eps)
+
+## Legal
+
+This work is licensed under a [Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License](http://creativecommons.org/licenses/by-nc-sa/4.0/).
